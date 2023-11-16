1ccd7110de15ca5ae301f94d8ab496d4bd4d69b3
Kicki Lindstrand <kicki.lindstrand@gmail.com>
2023-11-16 13:14:24 +0100

    hello world

--- server/functions/getMenu/index.js
+++ server/functions/getMenu/index.js
@@ -3,7 +3,7 @@ module.exports.handler = async (event) => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Function to get menu',
+        message: 'Hello world',
         input: event,
       },
       null,

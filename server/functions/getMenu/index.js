d0bea5fee91d86396d94cd457d1d751459135658
Kicki Lindstrand <kicki.lindstrand@gmail.com>
2023-11-16 14:13:51 +0100

    added functions db, getMenu, addToMenu

--- server/functions/getMenu/index.js
+++ server/functions/getMenu/index.js
@@ -1,13 +1,18 @@
-module.exports.handler = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Hello world',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+const { sendResponse } = require('../../responses/index')
+const { db } = require("../../services/db")
+
+module.exports.handler = async (event, context) => {
+  try {
+    const { items } = await db.scan({
+      TableName: "menuDb",
+      FilterExpression: "attribute_exists(#DYNOBASE_menu)",
+      ExpressionAttributeNames: {
+        "#DYNOBASE_menu": "menu"
+      }
+    }).promise();
+    return sendResponse(200, { success: true, menu: items });
+  } catch (error) {
+    return sendResponse(501, { error: error, message: "Somehing went wrong" })
+  }
+  
 };

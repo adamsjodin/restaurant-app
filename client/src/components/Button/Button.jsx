29a536c1159b36c0103f0070f62831ccf6ffa793
SamCcode <yhfe22.karlsa@folkuniversitetet.nu>
2023-11-17 09:40:36 +0100

    basic stylesheet setup

---
+++ client/src/components/Button/Button.jsx
@@ -0,0 +1,13 @@
+
+
+function Button({ children, onClick, className }) {
+
+  
+    return (
+        <button className={`button-primary ${className || ''}`} type="button" onClick={onClick}>
+        {children}
+      </button>
+    );
+  }
+  
+  export default Button;

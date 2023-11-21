7c9e6160b6f095a9612786eb1990454a7fae13f5
Kicki Lindstrand <kicki.lindstrand@gmail.com>
2023-11-21 09:43:43 +0100

    reservation component

--- client/src/components/Reservation/Reservation.jsx
+++ client/src/components/Reservation/Reservation.jsx
@@ -1,15 +1,40 @@
+import Form from "../Form/Form";
+import Input from "../Input/Input";
 import ReservationConfirmation from "./ReservationConfirmation";
 import "./reservation.scss"
 import { useState } from "react";
 
 export default function Reservation() {
   const [showConfirmation, setShowConfirmaion] = useState(false)
+  const [guests, setGuests] = useState(1)
+
   return (
     <article className="reservation">
       <img src="./logos/claddagh.png" ></img>
       <p>For reservations more than ten people please send us an email.</p>
+      {/*insert new form here*/}
+      <form
+            autoComplete='on'
+            className='form'
+        >
+            <section className="form__inputs">
+                <input className="input" placeholder="Date"/>
+                <input className="input" placeholder="Time"/>
+                <input className="input" placeholder="Email"/>
+                <input className="input" placeholder="Name" />
+                <input className="input" placeholder="Phone" />
+            </section>
+        </form>
+
+      <section className="reservation__nrOfGuests">
+        <div className="circle--large">{guests}</div>
+        <section className="circles">
+          <div className="circle--small" onClick={() => guests >= 2 ? setGuests(guests - 1) : guests}> - </div>
+          <div className="circle--small" onClick={() => guests <= 9 ? setGuests(guests + 1) : guests}>+</div>
+        </section>
+      </section>
       <button onClick={() => setShowConfirmaion(true)} className="btn button--primary">Confirm reservation</button>
-      {showConfirmation && <ReservationConfirmation action={setShowConfirmaion} /> }
+      {showConfirmation && <ReservationConfirmation action={setShowConfirmaion} guests={guests} /> }
     </article>
   )
 }

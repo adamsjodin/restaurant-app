2bbedaa0c485ea2823b6557b2192cbb19c03c37d
Kicki Lindstrand <kicki.lindstrand@gmail.com>
2023-11-22 09:30:48 +0100

    fixed userDb and functions

--- server/functions/getUser/index.js
+++ server/functions/getUser/index.js
@@ -3,13 +3,12 @@ const { db } = require("../../services/db")
 const bcrypt = require('bcryptjs')
 
 module.exports.handler = async (event) => {
-  const { email, password, role } = JSON.parse(event.body)
+  const { email, password } = JSON.parse(event.body)
   //Send email, password and role when logging in. Not possible... Check for role must happen later! Think again...  
   const params = {
     TableName: "userDb",
     Key: {
-      email: email,
-      role: role
+      email: email
     }
   }
   try {

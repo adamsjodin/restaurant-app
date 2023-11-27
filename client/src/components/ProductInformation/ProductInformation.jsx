d8d40226a79980bd070e07ff627381965436a617
SamCcode <yhfe22.karlsa@folkuniversitetet.nu>
2023-11-27 22:14:06 +0100

    cart in progress

--- client/src/components/ProductInformation/ProductInformation.jsx
+++ client/src/components/ProductInformation/ProductInformation.jsx
@@ -6,29 +6,27 @@ import { IoMdClose } from "react-icons/io";
 
 function ProductInformation({ props, onClick }) {
   return (
-    <motion.article 
-    className="product-info">
-      <figure
-        className="product-info__image"
-        style={{ backgroundImage: `url(${props.imgUrl})` }}
-      ></figure>
-      <section className="product-info__info">
-        <h3>{props.title}</h3>
-        <p>{props.description}</p>
-        <h3>Allergens:</h3>
-        <ul className="allergens">
-          {props.ingredients.map((ingredient, id) => (
-            <li key={id}>{ingredient}</li>
-          ))}
-        </ul>
-      </section>
-      {/* {showInfo ? <IoMdClose onClick={handleClick}/> : <FaInfo onClick={handleClick} />} */}
-
-      <IoMdClose className="info-btn" onClick={onClick} />
-    </motion.article>
+    <>
+      <motion.article className="product-info">
+        <figure
+          className="product-info__image"
+          style={{ backgroundImage: `url(${props.imgUrl})` }}
+        ></figure>
+        <section className="product-info__info">
+          <h3>{props.title}</h3>
+          <p>{props.description}</p>
+          <h3>Allergens:</h3>
+          <ul className="allergens">
+            {props.ingredients.map((ingredient, id) => (
+              <li key={id}>{ingredient}</li>
+            ))}
+          </ul>
+        </section>
+        <IoMdClose className="info-btn" onClick={onClick} />
+      </motion.article>
       <Button className="add">Add +</Button>
       <h3 className="product-info__price">{props.price} kr</h3>
-    </article>
+    </>
   );
 }
 
--- client/src/pages/Home/Components/ProductCard/ProductCard.jsx
+++ client/src/pages/Home/Components/ProductCard/ProductCard.jsx
@@ -7,45 +7,62 @@ import "./ProductCard.scss";
 import { motion } from "framer-motion";
 import { IoMdClose } from "react-icons/io";
 
-function ProductCard({ props }) {
-  const { title, description, price, imgUrl } = props;
+function ProductCard({ props, onClick, className, cartInfo, increase, decrease }) {
+  const { title, description, price, imgUrl, quantity } = props;
+  const dynamicStyle = className ? `product product--${className}` : "product";
 
   const [showInfo, setShowInfo] = useState(false);
   const cardRef = useRef(null);
-  
 
   const handleClick = () => {
-      setShowInfo(!showInfo);
+    setShowInfo(!showInfo);
   };
 
   return (
     <>
-      { showInfo ? (
-        <ProductInformation className="productInformation" props={props} onClick={handleClick} />
+      {showInfo ? (
+        <ProductInformation
+          className="productInformation"
+          props={props}
+          onClick={handleClick}
+        />
       ) : (
-    <motion.article className="product" ref={cardRef}>
-        <>
-          <motion.figure
-            className="product__image"
-            style={{ backgroundImage: `url(${imgUrl})` }}
-          ></motion.figure>
-          <motion.section 
-
-          className="product__info">
-            <h3>{title}</h3>
-            <Truncate inline title={description}>
-              {description}
-            </Truncate>
-            <section className="product__info--bottom">
-              <h3>{price} kr</h3>
-              <Button className="add">Add +</Button>
-            </section>
-          </motion.section>
-        <FaInfo onClick={handleClick}/>
-        </>
-      </motion.article>
+        <motion.article className={dynamicStyle} ref={cardRef}>
+          <>
+            <motion.figure
+              className="product__image"
+              style={{ backgroundImage: `url(${imgUrl})` }}
+            ></motion.figure>
+            <motion.section className="product__info">
+              <h3>{title}</h3>
+              {cartInfo ? (
+                <div className="cart-wrapper">
+                  <Button className="add" onClick={decrease}>-</Button>{" "}
+                  <p>Quantity:{" " + quantity}</p>{" "}
+                  <Button className="add" onClick={increase}>+</Button>{" "}
+                </div>
+              ) : (
+                <Truncate inline title={description}>
+                  {description}
+                </Truncate>
+              )}
+              <section className="product__info--bottom">
+                <h3>{price} kr</h3>
+                {cartInfo ? (
+                  <></>
+                ) : (
+                  <Button className="add" onClick={() => onClick(props)}>
+                    Add +
+                  </Button>
+                )}
+              </section>
+            </motion.section>
+            {cartInfo ? <></> : <FaInfo onClick={handleClick} />}
+          </>
+        </motion.article>
       )}
-  </>);
+    </>
+  );
 }
 
 export default ProductCard;

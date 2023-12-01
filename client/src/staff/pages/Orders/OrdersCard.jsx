e349f74c321a124ed5bb66a9c4cd6dbb12595404
Kicki Lindstrand <kicki.lindstrand@gmail.com>
2023-12-01 09:56:57 +0100

    orders-staff

--- client/src/staff/pages/Orders/OrdersCard.jsx
+++ client/src/staff/pages/Orders/OrdersCard.jsx
@@ -1,20 +1,29 @@
 import { FaCircle } from "react-icons/fa";
 import Button from "../../../components/Button/Button";
+import { IoMdClose } from "react-icons/io";
+import { useState } from "react";
+import { FaInfo } from "react-icons/fa";
 
 
 export default function OrdersCard({ order }) {
+  const [showInfo, setShowInfo] = useState(false)
   const convertedDate = new Date(order.TimeStamp * 1000).toString();
   const productEl = order.products.map((product, i) => {
-    return <div key={i}><h2>{product.title}, {product.quantity}</h2> <h2>Notes:</h2>{product.changes.map((change, i) => {return <p key={i}>{change}</p>})}</div>
+    return <section key={i}><p>{product.quantity}st {product.title} </p> <p>Notes:</p>{product.changes.map((change, i) => {return <p key={i}>{change}</p>})}<hr></hr></section>
   })
   return (
     <section className="order-history__card">
-      <FaCircle className={order.status === 'active' ? 'active' : ""}/>
-      <p>Order number: {order.orderNr}</p>
-      {productEl}
-      <p>{convertedDate.substring(4, 21)}</p>
-      
-    <Button children={"Done"} className={"add"}></Button>
+      <div><FaCircle className={order.status === 'active' ? 'active' : ""}/></div>
+      <div>
+        <p>Order number: {order.orderNr}</p>
+        <hr></hr>
+        {productEl}
+      </div>
+      <div>
+        <p>{convertedDate.substring(4, 21)}</p>
+        <Button children={"Done"} className={"add"}></Button>
+      </div>
+      {showInfo ? <IoMdClose /> : <FaInfo />}
     </section>
   );
 }

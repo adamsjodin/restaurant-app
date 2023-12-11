3b7a2313ee81d44ff5d05c93aadd967a05fdf4c8
Joakim Sundman <joakimsundman89@gmail.com>
2023-12-11 11:52:41 +0100

    fixed precheckout styling

--- client/src/components/PreCheckoutConfirmation/PreCheckoutConfirmation.jsx
+++ client/src/components/PreCheckoutConfirmation/PreCheckoutConfirmation.jsx
@@ -34,49 +34,55 @@ function PreCheckoutConfirmation({ cart, setAppState, appState, setCart }) {
         appState.openPreCheckout ? "visible" : "hidden"
       }`}
     >
-      <div className="pre-checkout__items-wrapper">
-        <div className="pre-checkout__top">
-          <h2>Your order</h2>
-          <p>Total Price: ${totalPrice.toFixed(2)}</p>
+      <div className="pre-checkout__top">
+        <h2>Your order</h2>
+        <p>Total Price: ${totalPrice.toFixed(2)}</p>
+      </div>
+      <section className="pre-checkout__bottom">
+        <div className="pre-checkout__items-wrapper">
+          {cart.map((item, i) => (
+            <section className="pre-checkout__items" key={i}>
+              <p>{item.quantity}</p>
+              <p>{item.title}</p>
+              <p>{item.price} kr</p>
+              {item.changes > 0
+                ? item.changes.map(([ingredient, changed]) => (
+                    <p className="changes" key={ingredient}>
+                      {changed ? "Add" : "Remove"} {ingredient}
+                    </p>
+                  ))
+                : null}
+            </section>
+          ))}
         </div>
-        {cart.map((item, i) => (
-          <section className="pre-checkout__items" key={i}>
-            <p>{item.quantity}</p>
-            <p>{item.title}</p>
-            <p>{item.price}</p>
-            {item.changes > 0 ? (
-  item.changes.map(([ingredient, changed]) => (
-    <p className="changes" key={ingredient}>
-      {changed ? "Add" : "Remove"} {ingredient}
-    </p>
-  ))
-) : null}
-          </section>
-        ))}
-      </div>
-      <div className="pre-checkout__btns">
-        {appState.openCheckout ? (
-          <>
-            <p style={{ fontSize: "1.5em", color: "limegreen" }}>Previous order in progress...</p>
-            <Button className="secondary" onClick={handleEditClick}>Back to cart</Button>
-          </>
-        ) : (
-          <>
-            <Button onClick={handleButtonClick}>Looks good</Button>
-            <Button className="secondary" onClick={handleEditClick}>
-              Edit my order{" "}
-              <Countdown
-                onTimeout={() => {
-                  oneState(setAppState, "openPreCheckout");
-                  postOrder(setCart);
-                }}
-                isCountdownActive={isCountdownActive}
-                duration={1}
-              />
-            </Button>
-          </>
-        )}
-      </div>
+        <div className="pre-checkout__btns">
+          {appState.openCheckout ? (
+            <>
+              <p style={{ fontSize: "1.5em", color: "limegreen" }}>
+                Previous order in progress...
+              </p>
+              <Button className="secondary" onClick={handleEditClick}>
+                Back to cart
+              </Button>
+            </>
+          ) : (
+            <>
+              <Button onClick={handleButtonClick}>Looks good</Button>
+              <Button className="secondary" onClick={handleEditClick}>
+                Edit my order{" "}
+                <Countdown
+                  onTimeout={() => {
+                    oneState(setAppState, "openPreCheckout");
+                    postOrder(setCart);
+                  }}
+                  isCountdownActive={isCountdownActive}
+                  duration={1}
+                />
+              </Button>
+            </>
+          )}
+        </div>
+      </section>
     </article>
   );
 }

c5a0415e41218769572a4077f9a9d3a134159c6b
SamCcode <yhfe22.karlsa@folkuniversitetet.nu>
2023-12-11 09:32:29 +0100

    hours in progress

--- client/src/staff/pages/OpeningHours/OpeningHours.jsx
+++ client/src/staff/pages/OpeningHours/OpeningHours.jsx
@@ -4,90 +4,109 @@ import { Button, Input } from "../../../components/exports";
 import axios from "axios";
 
 function OpeningHours() {
-
-  // const [Hours, setHours] = useState([]);
-
-    useEffect(() => {
-        axios.get('https://khmfpjooy4.execute-api.eu-north-1.amazonaws.com/api/staff/hours')
-        .then(res => console.log(res.data.hours))
-    }, [])
-
-    const handleSubmit = (e) => {
-      e.preventDefault()
-      axios.put('https://khmfpjooy4.execute-api.eu-north-1.amazonaws.com/api/hours', Hours)
-          .then(res => {
-              console.log('updated db', updateMenu);
-              updateMsg();
-              return res;
-          })
-          .catch(err => console.error(err))
-  }
-
-  const initialHours = [
-    "2pm - 10pm*",
-    "2pm - 10pm*",
-    "2pm - 10pm*",
-    "2pm - 10pm*",
-    "2pm - 10pm*",
-    "2pm - 10pm*",
-    "Closed",
-  ];
-  const [note, setNote] = useState(
-    "*Please note that the kitchen closes 30 minutes earlier."
-  );
-
-  const [hours, setHours] = useState(initialHours);
+  const [hours, setHours] = useState([]);
+  const [note, setNote] = useState("");
   const [editMode, setEditMode] = useState(false);
+  const daysOrder = [
+    "monday",
+    "tuesday",
+    "wednesday",
+    "thursday",
+    "friday",
+    "saturday",
+    "sunday",
+  ];
+  const sortedHours = hours
+    .slice()
+    .sort((a, b) => daysOrder.indexOf(a.days) - daysOrder.indexOf(b.days));
 
-  const handleEditClick = () => {
-    setEditMode(!editMode);
+  useEffect(() => {
+    axios
+      .get(
+        "https://khmfpjooy4.execute-api.eu-north-1.amazonaws.com/api/staff/hours"
+      )
+      .then((res) => {
+        setHours(res.data.hours);
+        if (res.data.hours.some((item) => item.notes)) {
+          setNote(res.data.hours.find((item) => item.notes).notes);
+        }
+      })
+      .catch((err) => console.error(err));
+  }, []);
+
+// kan inte skicka hela listan utan ett obj i taget
+
+  const handleSubmit = () => {
+    axios
+      .put(
+        "https://khmfpjooy4.execute-api.eu-north-1.amazonaws.com/api/staff/hours",
+        hours
+      )
+      .then((res) => {
+        return res;
+      })
+      .catch((err) => console.error(err));
   };
 
-  const handleInputChange = (index, event, type) => {
-    if (type === "hours") {
-      const newHours = [...hours];
-      newHours[index] = event.target.value;
-      setHours(newHours);
-    } else if (type === "note") {
-      setNote(event.target.value);
+  const handleEditClick = () => {
+    if (editMode) {
+       handleSubmit()
+      setEditMode(!editMode);
+    } else {
+      setEditMode(!editMode);
     }
   };
 
-  const renderHoursList = () => {
-    return hours.map((hour, index) => (
-      <li key={index}>
-        {editMode ? (
-          <Input
-            type="text"
-            value={hour}
-            onChange={(event) => handleInputChange(index, event, "hours")}
-          />
-        ) : (
-          hour
-        )}
-      </li>
-    ));
+  const handleInputChange = (id, event, type) => {
+    console.log("Input Change:", id, event.target.value);
+    if (type === "hours") {
+      setHours((prevHours) => {
+        const updatedHours = prevHours.map((hour) =>
+          hour.id === id ? { ...hour, hours: event.target.value } : hour
+        );
+        return updatedHours;
+      });
+    } else if (type === "note") {
+      setNote(event.target.value);
+      setHours((prevHours) => {
+        const updatedHours = prevHours.map((hour) =>
+          hour.notes !== undefined ? { ...hour, notes: event.target.value } : hour
+        );
+        return updatedHours;
+      });
+    }
   };
 
   return (
     <section className="hours-wrapper__staff">
       <section className="hours--lists">
         <ul className="hours--ul">
-          <li>Monday:</li>
-          <li>Tuesday:</li>
-          <li>Wednesday:</li>
-          <li>Thursday:</li>
-          <li>Friday:</li>
-          <li>Saturday:</li>
-          <li>Sunday:</li>
+          {sortedHours.map((hour, index) => (
+           <li key={hour.id}>
+           {editMode && hour.hours !== undefined ? (
+             <div key={`edit-${hour.id}`}>
+               <label htmlFor={`hoursInput-${hour.id}`} key={`label-${hour.id}`}>
+                 {hour.days}:
+               </label>
+               <Input
+                 id={`hoursInput-${hour.id}`}
+                 type="text"
+                 placeholder={hour.hours}
+                 onChange={(event) => handleInputChange(hour.id, event, "hours")}
+                 key={`input-${hour.id}`}
+               />
+             </div>
+           ) : (
+             hour.hours && `${hour.days}: ${hour.hours}`
+           )}
+         </li>
+          ))}
         </ul>
-
-        <ul className="hours--ul">{renderHoursList()}</ul>
       </section>
       <p>
         {editMode ? (
           <textarea
-            value={note}
+            placeholder={note}
             onChange={(event) => handleInputChange(null, event, "note")}
           />
         ) : (
